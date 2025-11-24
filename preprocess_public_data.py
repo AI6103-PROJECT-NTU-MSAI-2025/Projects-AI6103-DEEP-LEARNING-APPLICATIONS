71c35c55a4b27fa682bb7c40f69b2c254bd5959e
柏森 <2417508347@qq.com>
2025-11-24 20:27:36 +0800

    clean code base and update readme

--- preprocess_public_data.py
+++ preprocess_public_data.py
@@ -5,8 +5,8 @@ def main():
     text_embedding_model = "blair"
     #text_embedding_model = "openai" #use OpenAI text-embedding-3-large embeddings instead of BLaIR
 
-    get_common_preprocessors(text_embedding_model=text_embedding_model)["amzn23_office"].preprocess_rating()
-    get_common_preprocessors(text_embedding_model=text_embedding_model)["amzn23_game"].preprocess_rating()
+    # get_common_preprocessors(text_embedding_model=text_embedding_model)["amzn23_office"].preprocess_rating()
+    # get_common_preprocessors(text_embedding_model=text_embedding_model)["amzn23_game"].preprocess_rating()
     get_common_preprocessors(text_embedding_model=text_embedding_model)["amzn23_music"].preprocess_rating()
 if __name__ == "__main__":
     main()
